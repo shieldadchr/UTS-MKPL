f70ce1a82b4d3d7437f30142304a2880dcd6d910
primayazadi <primayazadi10@gmail.com>
2024-11-04 13:43:04 +0700

    updatepass

--- Database/src/users/users.service.js
+++ Database/src/users/users.service.js
@@ -73,5 +73,20 @@ const deleteUser = async (username) => {
   return { message: `User ${username} has been deleted successfully` };
 };
 
-// TAMBAHAN: Ekspor fungsi deleteUser
-export { createUser, loginUser, editUsersByName, getUser, getAllUsers, deleteUser };
+const updatePassword = async (username, newPassword) => {
+  const user = await findUsersByUsername(username);
+  if (!user) {
+    throw new Error(`User ${username} not found`);
+  }
+
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+  await prisma.Users.update({
+    where: { username: username },
+    data: { password: hashedPassword },
+  });
+
+  return { message: `Password for ${username} has been updated successfully` };
+};
+
+// TAMBAHAN: Ekspor fungsi updatePassword
+export { createUser, loginUser, editUsersByName, getUser, getAllUsers, deleteUser, updatePassword };
